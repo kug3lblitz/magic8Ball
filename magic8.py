7ccfcdc571b9dddcd3b45b4413bbc90be0fdb32c
Patrick Werner <pwerner2@gmail.com>
2023-03-28 14:43:54 -0400

    python version

---
+++ magic8.py
@@ -0,0 +1,34 @@
+# magic 8 ball
+import random, time
+
+answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+]
+
+question = input("What is your question? ")
+print("Thinking...")
+# 5 second delay with dots
+for i in range(5):
+    print(".")
+    time.sleep(1)
+
+print(random.choice(answers))
